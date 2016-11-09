b65890fe21613b9bbc217aab0f3816891dda69d0
Daudex <davidbary@msn.com>
2016-11-09 21:12:01 +0000

    Update obstacle_avoid_robot.ino

--- obstacle_avoid_robot.ino
+++ obstacle_avoid_robot.ino
@@ -1,6 +1,12 @@
 //Defining the pins before the setup
-#define echopin  2
-#define trigpin 3
+#define echopin 9
+#define trigpin 8
+#define motorLeft2 3
+#define motorRight2 5
+#define motorLeft1 6
+#define motorRight1 7
+
+
 
 int maximumRange = 30;
 long duration, distance;
@@ -8,11 +14,10 @@ void setup() {
   Serial.begin (9600);   // setting up the serial communication
   pinMode (trigpin, OUTPUT);       // setting the Input / Output in setup
   pinMode (echopin, INPUT );
-  pinMode (4, OUTPUT);     // pins for motor movement
-  pinMode (5, OUTPUT);
-  pinMode (13, OUTPUT);
-  pinMode (6, OUTPUT);
-  pinMode (7, OUTPUT);
+  pinMode (motorLeft1, OUTPUT);     // pins for motor movement
+  pinMode (motorLeft2, OUTPUT);
+  pinMode (motorRight1, OUTPUT);
+  pinMode (motorRight2, OUTPUT);
   }
   void loop ()
   {
@@ -27,29 +32,30 @@ void setup() {
       Serial.println(distance);
       }
       if (distance >= 30 ){
-        digitalWrite(4,HIGH);
-        digitalWrite(5,HIGH);
-        digitalWrite(6,LOW);
-        digitalWrite(7,LOW);
+        digitalWrite(motorLeft1,HIGH);
+        digitalWrite(motorRight1,HIGH);
+        digitalWrite(motorLeft2,LOW);
+        digitalWrite(motorRight2,LOW);
         delay (200);
         }
         else if (distance >=15 && distance <= 25) {
-          digitalWrite (4,HIGH);
-          digitalWrite (5,LOW);
-          digitalWrite (6,LOW);
-          digitalWrite (7,LOW);
+          digitalWrite (motorLeft1,HIGH);
+          digitalWrite (motorRight1,LOW);
+          digitalWrite (motorLeft2,LOW);
+          digitalWrite (motorRight2,LOW);
           delay (1000);
           }
           else if (distance < 15){
-            digitalWrite (4, LOW);
-            digitalWrite (5, LOW);
-            digitalWrite (6,HIGH);
-            digitalWrite (7,HIGH);
+            digitalWrite (motorLeft1, LOW);
+            digitalWrite (motorRight1, LOW);
+            digitalWrite (motorLeft2,HIGH);
+            digitalWrite (motorRight2,HIGH);
             delay (1000);
-            digitalWrite (4,LOW);
-            digitalWrite (5,HIGH);
-            digitalWrite (6,LOW);
-            digitalWrite (7, LOW);
+            digitalWrite (motorLeft1,LOW);
+            digitalWrite (motorRight1,HIGH);
+            digitalWrite (motorLeft2,LOW);
+            digitalWrite (motorRight2, LOW);
             delay (1000);
-            }
-            }
+          }
+          
+          }
